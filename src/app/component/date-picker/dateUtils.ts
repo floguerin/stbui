c01b635d6111cbf327f28f159825d0674b3f3db8
Florian Guerin <florianguerin@outlook.com>
2018-05-06 14:52:36 +0200

    sources

--- src/app/component/date-picker/dateUtils.ts
+++ src/app/component/date-picker/dateUtils.ts
@@ -7,20 +7,20 @@ let localConfig = {
   dayList: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
   monthList: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
   monthLongList: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
-}
+};
 
 export const dateTimeFormat = {
   formatDisplay (date) {
-    var day = date.getDate()
+    var day = date.getDate();
     return `${localConfig.monthList[date.getMonth()]}-${day > 9 ? day : '0' + day} ${localConfig.dayList[date.getDay()]}`
   },
   formatMonth (date) {
     return `${date.getFullYear()} ${localConfig.monthLongList[date.getMonth()]}`
   },
   getWeekDayArray (firstDayOfWeek) {
-    let beforeArray = []
-    let afterArray = []
-    const dayAbbreviation = localConfig.dayAbbreviation
+    let beforeArray = [];
+    let afterArray = [];
+    const dayAbbreviation = localConfig.dayAbbreviation;
     for (let i = 0; i < dayAbbreviation.length; i++) {
       if (i < firstDayOfWeek) {
         afterArray.push(dayAbbreviation[i])
@@ -30,13 +30,13 @@ export const dateTimeFormat = {
     }
     return beforeArray.concat(afterArray)
   }
-}
+};
 
 export function getDaysInMonth (d) {
-  const resultDate = getFirstDayOfMonth(d)
+  const resultDate = getFirstDayOfMonth(d);
 
-  resultDate.setMonth(resultDate.getMonth() + 1)
-  resultDate.setDate(resultDate.getDate() - 1)
+  resultDate.setMonth(resultDate.getMonth() + 1);
+  resultDate.setDate(resultDate.getDate() - 1);
 
   return resultDate.getDate()
 }
@@ -46,52 +46,52 @@ export function getFirstDayOfMonth (d) {
 }
 
 export function getWeekArray (d, firstDayOfWeek) {
-  const dayArray = []
-  const daysInMonth = getDaysInMonth(d)
-  const weekArray = []
-  let week = []
+  const dayArray = [];
+  const daysInMonth = getDaysInMonth(d);
+  const weekArray = [];
+  let week = [];
 
   for (let i = 1; i <= daysInMonth; i++) {
     dayArray.push(new Date(d.getFullYear(), d.getMonth(), i))
   }
 
   const addWeek = (week) => {
-    const emptyDays = 7 - week.length
+    const emptyDays = 7 - week.length;
     for (let i = 0; i < emptyDays; ++i) {
       week[weekArray.length ? 'push' : 'unshift'](null)
     }
     weekArray.push(week)
-  }
+  };
 
   dayArray.forEach((day) => {
     if (week.length > 0 && day.getDay() === firstDayOfWeek) {
-      addWeek(week)
+      addWeek(week);
       week = []
     }
-    week.push(day)
+    week.push(day);
     if (dayArray.indexOf(day) === dayArray.length - 1) {
       addWeek(week)
     }
-  })
+  });
 
   return weekArray
 }
 
 export function addDays (d, days) {
-  const newDate = cloneDate(d)
-  newDate.setDate(d.getDate() + days)
+  const newDate = cloneDate(d);
+  newDate.setDate(d.getDate() + days);
   return newDate
 }
 
 export function addMonths (d, months) {
-  const newDate = cloneDate(d)
-  newDate.setMonth(d.getMonth() + months)
+  const newDate = cloneDate(d);
+  newDate.setMonth(d.getMonth() + months);
   return newDate
 }
 
 export function addYears (d, years) {
-  const newDate = cloneDate(d)
-  newDate.setFullYear(d.getFullYear() + years)
+  const newDate = cloneDate(d);
+  newDate.setFullYear(d.getFullYear() + years);
   return newDate
 }
 
@@ -100,21 +100,21 @@ export function cloneDate (d) {
 }
 
 export function cloneAsDate (d) {
-  const clonedDate = cloneDate(d)
-  clonedDate.setHours(0, 0, 0, 0)
+  const clonedDate = cloneDate(d);
+  clonedDate.setHours(0, 0, 0, 0);
   return clonedDate
 }
 
 export function isBeforeDate (d1, d2) {
-  const date1 = cloneAsDate(d1)
-  const date2 = cloneAsDate(d2)
+  const date1 = cloneAsDate(d1);
+  const date2 = cloneAsDate(d2);
 
   return (date1.getTime() < date2.getTime())
 }
 
 export function isAfterDate (d1, d2) {
-  const date1 = cloneAsDate(d1)
-  const date2 = cloneAsDate(d2)
+  const date1 = cloneAsDate(d1);
+  const date2 = cloneAsDate(d2);
 
   return (date1.getTime() > date2.getTime())
 }
@@ -132,10 +132,10 @@ export function isEqualDate (d1, d2) {
 }
 
 export function monthDiff (d1, d2) {
-  let m
-  m = (d1.getFullYear() - d2.getFullYear()) * 12
-  m += d1.getMonth()
-  m -= d2.getMonth()
+  let m;
+  m = (d1.getFullYear() - d2.getFullYear()) * 12;
+  m += d1.getMonth();
+  m -= d2.getMonth();
   return m
 }
 
